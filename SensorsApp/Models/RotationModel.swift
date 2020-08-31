94630ff4cd282d977e6499f6a25408417e44a01e
Gundars Kokins <c-gundars.kokins@charter.com>
2020-08-31 20:08:12 +0300

    missing files

---
+++ SensorsApp/Models/RotationModel.swift
@@ -0,0 +1,19 @@
+//
+//  RotationModel.swift
+//  SensorsApp
+//
+//  Created by Gundars Kokins on 31/08/2020.
+//  Copyright © 2020 Gundars Kokins. All rights reserved.
+//
+
+import Foundation
+
+struct RotationModel {
+    var pitch = 0.0
+    var roll = 0.0
+    var yaw = 0.0
+    
+    var rotationRateX = 0.0
+    var rotationRateY = 0.0
+    var rotationRateZ = 0.0
+}
